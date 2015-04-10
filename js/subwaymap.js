f4dc22bde1726d5f9ea1561ec24969cafe640185
Dave Brown <davidbrown@g.harvard.edu>
2015-04-10 13:18:27 -0400

    added simple map to tease out the format

--- js/subwaymap.js
+++ js/subwaymap.js
@@ -1,17 +1,28 @@
-SubwayMap = function(stationGraphData, eventHandler) {
+/**
+ * subway map object for HW3 of CS171
+ * @param _parentElement -- the HTML or SVG element (D3 node) to which to attach the vis
+ * @param _stationGraph -- the data array
+ * @param _metaData -- the meta-data / data description object
+ * @constructor
+ */
+SubwayMap = function(_parentElement, _data, _metaData){
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.metaData = _metaData;
+    this.displayData = [];
 
-  this.data = stationGraphData;
-  this.eventHandler = $(eventHandler);
+    // define all constants
+    this.margin = {top: 10, right: 10, bottom: 10, left: 10},
+    var style = window.getComputedStyle(this.parentElement.node(), null);
+	this.width = parseInt(style.getPropertyValue('width')) - this.margin.left - this.margin.right,
+    this.height = 600 - this.margin.top - this.margin.bottom;
 
-};
+    this.init();
+}
 
 /**
  *
  */
-/* SubwayMap.prototype.init = function() {
-  
-};
-
 SubwayMap.prototype.init = function() {
   
-}; */
+};
