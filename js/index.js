0767f8299a959ae04c0fdeedf10209668d548d37
davepsilon <davidbrown@g.harvard.edu>
2015-04-21 14:22:04 -0400

    merged to index
    added map to index

--- js/index.js
+++ js/index.js
@@ -1,79 +1,99 @@
 // MAKE CHART GLOBAL FOR NOW FOR DEBUGGING.
 var chart;
 
-(function() {
+$(function() {
 
-  /**
-   *
-   */
-  var init = function(timelineSummary, stationSummaries, stationSeries) {
-    // Generate an event hanlder to share between the modules.
-    var eventHandler = new Object();
+  // Load in our data sets asynchronously and call the init function when ready.
+  queue().defer(d3.json, 'processing/full_summary.json')
+	.defer(d3.json, 'processing/station_summary.json')
+    .defer(d3.json, 'processing/station_series.json')
+	.defer(d3.json, 'processing/station_map.json')
+	.defer(d3.json, 'processing/line_blue.json')
+	.defer(d3.json, 'processing/line_orange.json')
+	.defer(d3.json, 'processing/line_green.json')
+	.defer(d3.json, 'processing/line_greenB.json')
+	.defer(d3.json, 'processing/line_greenC.json')
+	.defer(d3.json, 'processing/line_greenD.json')
+	.defer(d3.json, 'processing/line_greenE_underground.json')
+	.defer(d3.json, 'processing/line_greenE.json')
+	.defer(d3.json, 'processing/line_red.json')
+	.defer(d3.json, 'processing/line_redB.json')
+	.defer(d3.json, 'processing/line_redM.json')
+    .await(init);
+  
+  // error should be "null" if no error
+  function init(error, timelineSummary, stationSummaries, stationSeries, station_map, line_blue, line_orange, line_green, 
+					line_greenB, line_greenC, line_greenD, line_greenE_underground, line_greenE, line_red, line_redB, line_redM) {
+    if (!error) {
 
-    var timeline = new Timeline(
-      {
-        parent: '#timeline-container',
-        chart: '#timeline',
-      },
-      timelineSummary,
-      eventHandler
-    );
-    timeline.init();
+		//Create an eventHandler placeholder (to be filled in later)
+		var MyEventHandler = new Object();
 
-    var map = new Map('map', stationSummaries, eventHandler);
-    map.init();
+				// Instantiate map
+		var mapScale = 0.33;
+		var subwayMap = new SubwayMap(d3.select("#subwaymap"), mapScale, station_map, line_blue, line_orange, line_green, line_greenB, line_greenC, line_greenD, line_greenE_underground, line_greenE, line_red, line_redB, line_redM, MyEventHandler);
 
-    var chartElements = {
-      parent: '#chart-container',
-      graph: '#chart',
-      title: '#chart-title',
-      legend: '#chart-legend',
-      y_axis: '#chart-y-axis',
-      x_axis: '#chart-x-axis',
-      y_axis_title: '#chart-y-axis-title',
-      x_axis_title: '#chart-x-axis-title',
-    };
+		
+		var timeline = new Timeline(
+		  {
+			parent: '#timeline-container',
+			chart: '#timeline',
+		  },
+		  timelineSummary,
+		  MyEventHandler
+		);
+		timeline.init();
 
-    var chartOptions = {
-      height: 270,
-      width: d3.select(chartElements.parent)[0][0].clientWidth,
-      title: {
-        height: 30,
-      },
-      x_axis: {
-        height: 25,
-      },
-      x_axis_title: {
-        height: 25,
-      },
-      y_axis: {
-        width: 80,
-      },
-    };
 
-    chart = new Chart(chartElements, chartOptions, stationSeries, eventHandler);
-    chart.init(1052, 0);
+		var chartElements = {
+		  parent: '#chart-container',
+		  graph: '#chart',
+		  title: '#chart-title',
+		  legend: '#chart-legend',
+		  y_axis: '#chart-y-axis',
+		  x_axis: '#chart-x-axis',
+		  y_axis_title: '#chart-y-axis-title',
+		  x_axis_title: '#chart-x-axis-title',
+		};
 
-    var summary = new Summary({
-      root: 'summary',
-      snow_slider: 'snow-slider',
-    },
-    stationSummaries, eventHandler);
+		var chartOptions = {
+		  height: 270,
+		  width: d3.select(chartElements.parent)[0][0].clientWidth,
+		  title: {
+			height: 30,
+		  },
+		  x_axis: {
+			height: 25,
+		  },
+		  x_axis_title: {
+			height: 25,
+		  },
+		  y_axis: {
+			width: 80,
+		  },
+		};
 
-    summary.init();
+		chart = new Chart(chartElements, chartOptions, stationSeries, MyEventHandler);
+		chart.init(1052, 0);
+
+		var summary = new Summary({
+		  root: 'summary',
+		  snow_slider: 'snow-slider',
+		},
+		stationSummaries, MyEventHandler);
+
+		summary.init();
+			
+		// bind the eventHandler to the things related to selecting in the map
+		$(MyEventHandler).bind("selectionChange", function(event){
+			// TODO
+			//anotherVis.onSelectionChange(startDate, endDate);
+		});
+					
+    }
   };
 
-  // Load in our data sets and call the init function when ready.
-  //queue()
-  //  .defer(d3.json, 'processing/station_summary.json')
-  //  .defer(d3.json, 'processing/station_series.json')
-  //  .await(init);
-  d3.json('processing/full_summary.json', function(timelineSummary) {
-    d3.json('processing/station_summary.json', function(stationSummaries) {
-      d3.json('processing/station_series.json', function(stationSeries) {
-        init(timelineSummary, stationSummaries, stationSeries);
-      });
-    });
-  });
 
-})();
+});
+			
+			
